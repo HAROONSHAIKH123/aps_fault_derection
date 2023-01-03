fdeba62ab9d1ce4e01c58f5d5f5f82d9b7140eea
HAROONSHAIKH123 <hrrnshaikh@gmail.com>
2023-01-03 21:41:49 +0530

    Data ingestion completed

--- setup.py
+++ setup.py
@@ -15,11 +15,14 @@ def get_requirements()->List[str]:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
 
+
+
 setup(
-    name = "sensor",
-    version = "0.0.1",
-    author = "HAROONSHAIKH123",
-    author_email = "hrrnshaikh@gmail.com",
+    name="sensor",
+    version="0.0.2",
+    author="ineuron",
+    author_email="avnish@ineuron.ai",
     packages = find_packages(),
-    install_requires = get_requirements(),
+    install_requires=get_requirements(),
 )
+
